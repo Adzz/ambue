e453ba15d61444131f3e300482ae7ecfd195eb4a
Adam Lancaster <lancaster28@hotmail.com>
2020-10-05 11:19:05 +0100

    Format

--- lib/ambue_web/router.ex
+++ lib/ambue_web/router.ex
@@ -2,22 +2,22 @@ defmodule AmbueWeb.Router do
   use AmbueWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {AmbueWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {AmbueWeb.LayoutView, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", AmbueWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    live "/", PageLive, :index
+    live("/", PageLive, :index)
   end
 
   # Other scopes may use custom stacks.
@@ -36,8 +36,8 @@ defmodule AmbueWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: AmbueWeb.Telemetry
+      pipe_through(:browser)
+      live_dashboard("/dashboard", metrics: AmbueWeb.Telemetry)
     end
   end
 end
